3d9b0209d4638a54077f1082bf594976e3763f34
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-18 23:18:06 +0200

    hero na home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,21 @@
 import React from "react";
+import pigHero from "../assets/pig-hero.png";
 
 function HomePage() {
     return (
-        <p>Tempor dolore reprehenderit velit consectetur et in. Qui dolor mollit tempor aliquip ea aute occaecat nisi voluptate magna amet do. Sunt deserunt officia consectetur Lorem excepteur Lorem nisi nisi. Dolore irure nisi laborum sunt nulla. Tempor laborum reprehenderit excepteur do Lorem laboris tempor duis veniam veniam proident. Do culpa do elit nostrud.
-
-Adipisicing culpa proident incididunt qui elit nisi minim do ipsum est minim adipisicing labore. Laborum incididunt veniam adipisicing quis cillum ullamco in. Qui officia nisi aliquip elit velit deserunt nisi mollit quis sunt qui deserunt elit culpa. Nostrud irure cillum ex minim. Enim culpa ipsum anim ex laborum velit aute esse consectetur aliquip sunt et mollit sint. Id sunt nulla fugiat deserunt eu est id in anim.
-
-Occaecat non ipsum esse velit do veniam culpa. Ad ipsum dolore aliquip duis ut nisi elit magna. Et non sint sunt laboris adipisicing ea occaecat laborum. Irure ex commodo fugiat occaecat dolor et. Voluptate enim eu aute tempor amet cillum non ea fugiat enim nulla aliquip. Aliquip tempor fugiat do cillum officia anim nostrud consectetur in. Qui excepteur incididunt proident consectetur dolor magna veniam dolore eiusmod anim non do.
-
-Ea nisi tempor aliqua culpa Lorem veniam. Dolor velit ad nostrud eiusmod amet enim aliquip consectetur. Commodo eu laborum laboris Lorem. Aliqua cillum quis sunt dolore ullamco dolor amet minim do aliqua consequat fugiat. Reprehenderit excepteur cillum qui aliqua in nulla sint mollit cupidatat mollit do veniam. Sunt sint ipsum anim officia proident dolore anim incididunt ex ex quis reprehenderit. Duis cupidatat ea tempor nisi minim nostrud reprehenderit anim culpa.
-
-Nisi labore qui officia elit in eu sit proident ea proident ut mollit ea est. Incididunt mollit qui esse qui ut eu aute qui velit. Cupidatat laborum officia veniam anim dolor ex aute ad non. Pariatur cillum elit enim nulla adipisicing et nostrud laboris anim aliqua aliqua id non.</p>
+        <main className="page">
+            <div className="content">
+                <section className="hero">
+                    <div className="hero-left">
+                        <h1>Związek Hodowców <br />
+                            Kawii Rasowych</h1>
+                        <p>Aliqua reprehenderit mollit aliquip ea velit culpa nisi dolor sit aliquip elit. Cupidatat fugiat elit non reprehenderit minim laborum adipisicing elit ex eu incididunt nisi ad. Consequat commodo do incididunt velit. Nostrud occaecat nisi laborum laboris nostrud incididunt.</p>
+                        <div className="buttons"></div>
+                    </div>
+                    <img src={pigHero} alt="Świnka morska"></img>
+                </section>
+            </div>
+        </main>
     );
 }
 
