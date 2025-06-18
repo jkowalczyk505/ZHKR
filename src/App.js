62324e3edca7470bf2e2f020c3545196ac481501
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-18 15:08:23 +0200

    szkielet

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles/App.scss";
+import routes from "./routes";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    {routes.map((route, index) => (
+                        <Route key={index} path={route.path} element={route.element} />
+                    ))}
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+
+export default App;
