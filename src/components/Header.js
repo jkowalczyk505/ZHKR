bdfe67194329eb1e84c4171123906ae5bb777333
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-18 16:39:07 +0200

    kawałek headera

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Header() {
     return (
-        <p>Header</p>
+        <header className="header">
+            <img src={logo} alt="logo"></img>
+            
+            <nav id="main-nav">
+                <ul className="menu">
+                    <li className="menu-item">
+                        <NavLink to="/" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}>Strona główna</NavLink>
+                    </li>
+                    <li className="menu-item">
+                        <NavLink to="/hodowle" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}>Nasze hodowle</NavLink>
+                    </li>
+                    <li className="menu-item">
+                        <NavLink to="/o-nas" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}>O nas</NavLink>
+                    </li>
+                    <li className="menu-item">
+                        <NavLink to="/kontakt" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}>Kontakt</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
