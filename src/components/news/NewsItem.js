ebd00f8678f06a85e9a5467aed428c5d07ae33be
Emilia <afra57@onet.pl>
2025-06-20 14:07:54 +0200

    poprawa news item - przycisk stale miejsce

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -4,31 +4,32 @@ import { IoNewspaperOutline } from "react-icons/io5";
 import Button from "../Button";
 
 function NewsItem({ title, description, date, image, slug }) {
-    return (
-        <div className="news-item">
-            {image ? (
-                <img src={image} alt={`Miniatura do: ${title}`} />
-            ) : (
-                <div className="news-placeholder">
-                    <IoNewspaperOutline className="placeholder-icon" />
-                </div>
-            )}
-
-            <div className="news-item-right">
-                <span className="news-item-date">{date}</span>
-                <h3 className="news-item-title">
-                <Link to={`/aktualnosci/${slug}`}>{title}</Link>
-                </h3>
-                <p className="news-item-description">{description}</p>
-
-                <div className="news-item-button">
-                <Button variant="outline" to={`/aktualnosci/${slug}`}>
-                    Czytaj więcej
-                </Button>
-                </div>
-            </div>
+  return (
+    <div className="news-item">
+      {image ? (
+        <img src={image} alt={`Miniatura do: ${title}`} />
+      ) : (
+        <div className="news-placeholder">
+          <IoNewspaperOutline className="placeholder-icon" />
         </div>
-    )
+      )}
+
+      <div className="news-item-right">
+        <div>
+          <span className="news-item-date">{date}</span>
+          <h3 className="news-item-title">
+            <Link to={`/aktualnosci/${slug}`}>{title}</Link>
+          </h3>
+          <p className="news-item-description">{description}</p>
+        </div>
+        <div className="news-item-button">
+          <Button variant="outline" to={`/aktualnosci/${slug}`}>
+            Czytaj więcej
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewsItem;
