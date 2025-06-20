77c8a6f0d703130bd6115199c65c70e016806fd3
Emilia <afra57@onet.pl>
2025-06-20 15:36:15 +0200

    animacje

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,37 +5,36 @@ import NewsSection from "../components/news/NewsSection";
 import BreedingCarousel from "../components/breedings/BreedingCarousel";
 
 function HomePage() {
-
   return (
     <main className="page">
-        <section className="hero dark-section">
-          <div className="hero-left">
-            <h1>
-              Związek Hodowców <br />
-              Kawii Rasowych
-            </h1>
-            <p>
-              Aliqua reprehenderit mollit aliquip ea velit culpa nisi dolor sit
-              aliquip elit. Cupidatat fugiat elit non reprehenderit minim
-              laborum adipisicing elit ex eu incididunt nisi ad. Consequat
-              commodo do incididunt velit. Nostrud occaecat nisi laborum laboris
-              nostrud incididunt.
-            </p>
-            <div className="buttons">
-              <Button variant="primary" to="/o-nas">
-                Dowiedz się więcej
-              </Button>
-              <Button variant="outline" to="/kontakt">
-                Kontakt
-              </Button>
-            </div>
+      <section className="hero dark-section">
+        <div className="hero-left slide-in-left">
+          <h1>
+            Związek Hodowców <br />
+            Kawii Rasowych
+          </h1>
+          <p>
+            Aliqua reprehenderit mollit aliquip ea velit culpa nisi dolor sit
+            aliquip elit. Cupidatat fugiat elit non reprehenderit minim laborum
+            adipisicing elit ex eu incididunt nisi ad. Consequat commodo do
+            incididunt velit. Nostrud occaecat nisi laborum laboris nostrud
+            incididunt.
+          </p>
+          <div className="buttons">
+            <Button variant="primary" to="/o-nas">
+              Dowiedz się więcej
+            </Button>
+            <Button variant="outline" to="/kontakt">
+              Kontakt
+            </Button>
           </div>
-          <img src={pigHero} alt="Świnka morska"></img>
-        </section>
+        </div>
+        <img src={pigHero} alt="Świnka morska" className="slide-in-right"></img>
+      </section>
 
-        <NewsSection />
+      <NewsSection />
 
-        <BreedingCarousel />
+      <BreedingCarousel />
     </main>
   );
 }
