f00d157cb01bb32d5efb8445ae487264d9429634
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-22 17:28:45 +0200

    wiedza dalej + nowy tile

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
-function Tile({ title, to, icon }) {
+function Tile({ title, to, icon, onClick, className }) {
   return (
-    <Link to={to} className="admin-tile">
+    <Link to={to} className={`admin-tile ${className}`} onClick={onClick}>
       <div className="icon">{icon}</div>
       <h2>{title}</h2>
     </Link>
