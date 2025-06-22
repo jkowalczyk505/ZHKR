632209e043baf9c280478d638998019d8ac27ce4
Emilia <afra57@onet.pl>
2025-06-22 14:54:59 +0200

    spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="spinner-wrapper">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
