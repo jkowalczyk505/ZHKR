27da19e2744910f82e2dac22cbc929df415327a5
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-22 20:09:14 +0200

    poradniki sekcje

---
+++ src/components/guides/Breeding.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Breeding() {
+  return (
+    <div className="guide-div">
+      <h2 className="guide-title with-line">Rozród</h2>
+      <div className="subjects-below">
+            <div className="subject-below">
+              <h3 className="subject-title">Rujka</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Żywienie</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Poród</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Opieka nad młodymi</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Oddzielanie</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Breeding;
---
+++ src/components/guides/Feeding.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Feeding() {
+  return (
+    <div className="guide-div">
+      <h2 className="guide-title with-line">Żywienie</h2>
+      <div className="subjects-below">
+            <div className="subject-below">
+              <h3 className="subject-title">Sianko</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Suszone zioła</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Zielonki i warzywa</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+            <div className="subject-below">
+              <h3 className="subject-title">Karma</h3>
+              <p className="subject-content">Commodo voluptate ipsum cillum cillum ipsum qui ex exercitation ad et tempor. Aute sunt deserunt culpa commodo cillum Lorem eiusmod non duis excepteur ea ea. Ipsum eiusmod commodo nulla voluptate veniam. Pariatur sint exercitation nostrud nisi laboris eu labore et dolore incididunt. Non non eu nostrud tempor deserunt reprehenderit non consectetur officia duis sit ipsum amet. Eu fugiat eiusmod cillum laborum adipisicing veniam non mollit enim magna.</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Feeding;
