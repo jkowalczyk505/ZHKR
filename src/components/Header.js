142e091de49280e535b10cfe77441bf8bb0ea836
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-06-23 22:09:28 +0200

    responsywny header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header className="header">
-      <img src={logo} alt="logo"></img>
+      <div className="logo-wrapper">
+        <img src={logo} alt="logo" />
+      </div>
 
-      <nav id="main-nav">
-        <ul className="menu">
+      <button
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle menu"
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+
+      <nav id="main-nav" className={menuOpen ? "open" : ""}>
+        <ul className="menu" onClick={() => setMenuOpen(false)}>
           <li className="menu-item">
             <NavLink
               to="/"
